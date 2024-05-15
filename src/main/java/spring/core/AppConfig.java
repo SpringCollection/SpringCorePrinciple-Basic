07799982dcbe575e22423fbb2155ab8e003e1cfe
zzu-yaaa <jy122032@gmail.com>
2024-05-15 16:52:11 +0900

    refactor: AppConfig 역할과 구현 분리

--- src/main/java/spring/core/AppConfig.java
+++ src/main/java/spring/core/AppConfig.java
@@ -1,6 +1,8 @@
 package spring.core;
 
+import spring.core.discount.DiscountPolicy;
 import spring.core.discount.FixDiscountPolicy;
+import spring.core.member.MemberRepository;
 import spring.core.member.MemberService;
 import spring.core.member.MemberServiceImpl;
 import spring.core.member.MemoryMemberRepository;
@@ -8,11 +10,22 @@ import spring.core.order.OrderService;
 import spring.core.order.OrderServiceImpl;
 
 public class AppConfig {
+
+    //역할고 구현을 분리
+    //메소드명ㅎ = 역할, return = 구현체
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private static DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
